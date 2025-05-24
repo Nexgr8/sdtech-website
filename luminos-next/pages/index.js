e3027a9221dd04e64530614fb16c9a7a2a3ef16c
Mohammed AlSerkall <mohammed@Mohammeds-MacBook-Air-3.local>
2025-05-24 16:12:51 +0300

    Add working homepage to fix 404 error

--- luminos-next/pages/index.js
+++ luminos-next/pages/index.js
@@ -1,8 +1,8 @@
 export default function Home() {
-    return (
-      <div style={{ fontFamily: 'Arial', padding: 40 }}>
-        <h1>Welcome to SD Tech</h1>
-        <p>Smart Energy & IoT Solutions for the Future</p>
-      </div>
-    );
-  }
+  return (
+    <div style={{ fontFamily: 'Arial', padding: '40px', textAlign: 'center' }}>
+      <h1>Welcome to SD Tech</h1>
+      <p>Smart Energy & IoT Solutions for the Future</p>
+    </div>
+  );
+}
