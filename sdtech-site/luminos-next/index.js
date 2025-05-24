7bb301696275d7d2aa335355a8de9c9841739dad
Mohammed AlSerkall <mohammed@Mohammeds-MacBook-Air-3.local>
2025-05-24 17:17:18 +0300

    Remove sdtech-site folder and finalize working build

--- sdtech-site/luminos-next/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ fontFamily: 'Arial', padding: '50px', textAlign: 'center' }}>
-      <h1>Welcome to SD Tech</h1>
-      <p>Smart Energy & IoT Solutions for the Future</p>
-    </div>
-  );
-}
